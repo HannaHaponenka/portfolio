e3cb11fe248c554d5f021d53a848061d8e2a347c
Piotr Smarkutskyi <spospo010@gmail.com>
2026-09-25 02:22:38 +0200

    Update video settings

--- src/App.tsx
+++ src/App.tsx
@@ -1364,6 +1364,7 @@ export default function App() {
             loop
             muted
             playsInline
+            controls
             style={{ width: '100%', height: 'auto', display: 'block' }}
           />
           <div className="absolute bottom-0 left-0 right-0 p-8 pointer-events-none">
